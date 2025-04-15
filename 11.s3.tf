1d1090cc492ebe685c9ca45c093b0667cf830c1f
swamykatreddi <katreddiswamy@gmail.com>
2025-04-15 11:39:34 +0530

    creating new s3

--- 11.s3.tf
+++ 11.s3.tf
@@ -7,3 +7,13 @@ resource "aws_s3_bucket" "awsb66-bucket-111" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket" "awsb66-bucket-222" {
+  bucket = "awsb66-bucket-222"
+  acl    = "private"
+
+  tags = {
+    Name        = "awsb66-bucket-222"
+    Environment = "Dev"
+  }
+}
